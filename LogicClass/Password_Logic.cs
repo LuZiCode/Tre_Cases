ee1df749741b494888a531491980ecd74a997ea4
LZ <lucasziegler@live.dk>
2022-02-16 12:17:37 +0100

    Færdig gjort min kode med en masse kommentere

---
+++ LogicClass/Password_Logic.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicClass
+{
+    public class Password_Logic
+    {
+        //Opretter en ny public int, som blivere refereret til inde i program.cs
+        public int HasSpecialChar(string adgangskode)
+        {
+            //Opretter en string, som indebærer alle specialle charectors
+            string specialChar = @"|!#$%&/()=?»«@£§€{}.-;~`'<>_,";
+            //Laver en ny string, som har formålet om at blive talt på hvor mange gange der opstår et forskelligt special char i stringen adgangskode
+            int numberofspecialchar = 0;
+            //en foreach loop, som gentages hver gang der opstår et special char i item som matcher specialchar
+            foreach (var item in specialChar)
+            {
+                //hvis adgangskoden indeholder item, så kør denne if sætning
+                if (adgangskode.Contains(item))
+                {
+                    //Pluser denne int hver gang if sætningen bliver opfyldt
+                    numberofspecialchar++;
+                }
+            }
+            //Sender værdien tilbage i program.cs, når foreach loopen ikke kan opfyldes længere
+            return numberofspecialchar;
+        }
+        //Opretter en ny public int, som blivere refereret til inde i program.cs
+        public int HasNumber(string adgangskode)
+        {
+            //Opretter en string, som indebærer alle tal
+            string numbers = @"1234567890";
+            //Laver en ny string, som har formålet om at blive talt på hvor mange gange der opstår et forskelligt special char i stringen adgangskode
+            int countofnumbers = 0;
+            //en foreach loop, som gentages hver gang der opstår et tal i item som matcher int numbers
+            foreach (var items in numbers)
+            {
+                //hvis adgangskoden indeholder items, så kør denne if sætning
+                if (adgangskode.Contains(items))
+                {
+                    //Pluser denne int hver gang if sætningen bliver opfyldt
+                    countofnumbers++;
+                }
+            }
+            //Sender værdien tilbage i program.cs, når foreach loopen ikke kan opfyldes længere
+            return countofnumbers;
+        }
+        //Opretter en ny public int, som blivere refereret til inde i program.cs
+        public int HasCapital(string adgangskode)
+        {
+            //Laver en ny string, som har formålet om at blive talt på hvor mange gange der opstår et CAPS tegn i stringen adgangskode
+            int counter = 0;
+            for (int i = 0; i < adgangskode.Length; i++)
+            {
+                //Hvis en karakter i adgangskode er CAPS så +1 til counter
+                if (char.IsUpper(adgangskode[i]))
+                {
+                    counter++;
+                }
+            }
+            return counter;
+        }
+    }
+}
--- Tre_Cases/program.cs
+++ Tre_Cases/program.cs
@@ -12,7 +12,9 @@ namespace Tre_Cases
     {
         static void Main(string[] args)
         {
+            //Opretter en ny boolean, som er falsk
             Boolean accepterellerik = false;
+            //opretter en Whilelykke, som kører hver gang booleanen er false (Hvilket den er indtil den bliver true)
             while (accepterellerik == false)
             {
 
@@ -26,11 +28,13 @@ namespace Tre_Cases
                 Console.WriteLine("");
                 Console.Write("Indtas din ønskede mulighed: ");
 
+                //Converter min var til char, så jeg kan køre en switch case
                 var mulighed = Convert.ToChar(Console.ReadLine().ToLower());
 
-
+                //Opretter en switchcase, som håndterer muligheds input
                 switch (mulighed)
                 {
+                    //Kører denne case, hvis input i mulighed er "1"
                     case '1':
                         string brugernavn;
                         string adgangskode;
@@ -42,25 +46,33 @@ namespace Tre_Cases
                         Console.Write("Indtast adgangskode: ");
                         adgangskode = Console.ReadLine();
 
+                        //Opretter en ny string, som håndterer txt filen "brugernavn.txt". "@" foran linket til filerne fortæller computeren at de specialle tegn ikke er en del af teksten
                         string filePathBrugernavn = @"C:\Users\luczie\Desktop\The Three Cases - CSharp\Tre_Cases\Data\brugernavn.txt";
+                        //Opretter en ny string, som håndterer txt filen "adgangskode.txt"
                         string filePathAdgangskode = @"C:\Users\luczie\Desktop\The Three Cases - CSharp\Tre_Cases\Data\adgangskode.txt";
 
+                        //Læs alle linjer fra stringen filePathbrugernavn, hvis den indebærer input fra brugernavn og alle linjer fra stringen FilePathAdgangskode indebærer-
+                        //input fra adgangskode, så kør denne if sætning
                         if (File.ReadAllLines(filePathBrugernavn).Contains(brugernavn) && File.ReadAllLines(filePathAdgangskode).Contains(adgangskode))
                         {
                             Console.Clear();
                             Console.WriteLine("Du er nu logget ind");
                             Console.WriteLine("Tryk Enter for at navigere ind i hovedmenuen");
+                            //Laver min boolean (som jeg navngav oppe i toppen af password programmet) til true, så den bryder ud af lykken
                             accepterellerik = true;
                             Console.ReadKey();
                         }
+                        //Hvis tilfældet er, at brugernavnet eller adgangskoden ikke passer til deres txt filer, så kør denne her
                         else
                         {
                             Console.WriteLine("");
                             Console.WriteLine("Brugernavnet eller adgangskoden er forkert!");
                             Console.WriteLine("Tryk Enter for at prøve igen");
+                            //Man ryger selfølgelig tilbage til main programmet, fordi "accepterellerik" stadig ikke er true, så lykken brydes ikke
                             Console.ReadKey();
                         }
                         break;
+                    //Kører denne case, hvis input i mulighed er "1"
                     case '2':
                         Console.Clear();
                         Console.WriteLine("Ændre din adgangskode");
@@ -72,45 +84,90 @@ namespace Tre_Cases
                         Console.Write("Indtast adgangskode: ");
                         adgangskode = Console.ReadLine();
 
+                        //Opretter en ny string, som håndterer txt filen "brugernavn.txt", og opretter en ny string, som håndterer txt filen "adgangskode.txt"
                         filePathBrugernavn = @"C:\Users\luczie\Desktop\The Three Cases - CSharp\Tre_Cases\Data\brugernavn.txt"; filePathAdgangskode = @"C:\Users\luczie\Desktop\The Three Cases - CSharp\Tre_Cases\Data\adgangskode.txt";
 
+                        //Læs alle linjer fra stringen filePathbrugernavn, hvis den indebærer input fra brugernavn og alle linjer fra stringen FilePathAdgangskode indebærer-
+                        //input fra adgangskode, så kør denne if sætning
                         if (File.ReadAllLines(filePathBrugernavn).Contains(brugernavn) && File.ReadAllLines(filePathAdgangskode).Contains(adgangskode))
                         {
-                            Boolean ChangePass = false;
-                            while (ChangePass == false)
+                            //Opretter en ny boolean, som er falsk
+                            Boolean ChangePassKrav = false;
+                            //opretter en Whilelykke, som kører hver gang booleanen er false (Hvilket den er indtil den bliver true)
+                            while (ChangePassKrav == false)
                             {
                                 Console.Clear();
                                 Console.Write("Skriv din nye adgangskode for at ændre den: ");
+                                //Opretter en ny string, som håndterer det nye password
                                 string newpass = Console.ReadLine();
-                                Console.Write("Gentag din nye adgangskode: ");
-                                string newpass2 = Console.ReadLine();
 
-                                if (newpass == newpass2)
+                                //Opretter en et nyt Object, som åbner klassen Password_Logic.cs
+                                LogicClass.Password_Logic tjekefterkrav_Object = new LogicClass.Password_Logic();
+                                //Opretter en ny int, som er går ind i min public string "HasSpecialChar" med værdien fra stringen newpass
+                                int hasSpecialChar = tjekefterkrav_Object.HasSpecialChar(newpass);
+                                //Opretter en ny int, som er går ind i min public string "HasNumber" med værdien fra stringen newpass
+                                int hasNumber = tjekefterkrav_Object.HasNumber(newpass);
+                                //Opretter en ny int, som er går ind i min public string "HasCapital " med værdien fra stringen newpass
+                                int hasCapital = tjekefterkrav_Object.HasCapital(newpass);
+
+                                //Hvis hasSpecialChar opfyldes mere end 1 gang, og hvis hasNumber opfyldes mere end 1 gang, og hvis hasCapital opfyldes mere end 1 gang, så kør denne if sætning
+                                if (hasSpecialChar > 1 && hasNumber > 1 && hasCapital > 1)
                                 {
-                                    File.WriteAllText(filePathAdgangskode, newpass2);
-                                    Console.WriteLine("Du har nu ændret kodeordet");
-                                    ChangePass = true;
+                                    //Laver ChangePassKrav til true, så den bryder ud af loopen
+                                    ChangePassKrav = true;
+                                    Console.WriteLine("");
+                                    Console.Write("Gentag din nye adgangskode: ");
+                                    //Laver en ny string, som håndterer gentagelse af den nye kode
+                                    string newpass2 = Console.ReadLine();
+                                    //Laver en ny bool, som er falsk
+                                    Boolean ChangePass = false;
+                                    //Laver en ny whileloop, kører så længe ChangePass er false (Hvilket den er indtil den bliver true)
+                                    while (ChangePass == false)
+                                    {
+                                    //Hvis newpass er det samme som newpass2, så kør denne
+                                    if(newpass == newpass2)
+                                    {
+                                        //Skriv alt tekst fra stringen newpass2 ind i filePathAdgangskode og overskriv alt derinde.  
+                                        File.WriteAllText(filePathAdgangskode, newpass2);
+                                        Console.WriteLine("Du har nu ændret kodeordet");
+                                        //Laver changepass til true, så man bryder ud af loopen
+                                        ChangePass = true;
+                                    }
+                                    //Hvis newpass ikke er det samme som newpass2, så kør denne her
+                                    else
+                                    {
+                                        Console.WriteLine("");
+                                        Console.WriteLine("Adgangskoden matcher ikke!");
+                                        Console.WriteLine("Tryk Enter for at prøve igen");
+                                        Console.ReadKey();
+                                    }
+                                    }
                                 }
+                                //Kør denne hvis kravene fra hasSpecialChar, hasNumber og hasCapital ikke opfyldes
                                 else
                                 {
                                     Console.Clear();
-                                    Console.WriteLine("Koden matcher ikke");
+                                    Console.WriteLine("Koden matcher ikke kodeoprettelses kravene");
                                     Console.WriteLine("Tryk Enter for at prøve igen");
                                     Console.ReadKey();
                                 }
                             }
                             Console.ReadKey();
                         }
+                        //Hvis adgangskoden ikke opfylder stringen filePathadgangskode eller hvis brugernavnet ikke opfylder stringen filePathBrugernavn
                         else
                         {
                             Console.WriteLine("");
                             Console.WriteLine("Brugernavnet eller adgangskoden er forkert!");
-                            Console.WriteLine("Tryk Enter for at prøve igen");
+                            Console.WriteLine("Tryk Enter for at vende tilbage");
                             Console.ReadKey();
                         }
                         break;
+                    //Kører denne case, hvis input i mulighed er "1"
                     case '3':
+                        //Opretter en Boolean, som er falsk
                         Boolean adgangskodekrav = false;
+                        //opretter en Whilelykke, som kører hver gang booleanen er false (Hvilket den er indtil den bliver true)
                         while (adgangskodekrav == false)
                         {
                             Console.Clear();
@@ -122,30 +179,45 @@ namespace Tre_Cases
                             Console.Write("Indtast adgangskode: ");
                             adgangskode = Console.ReadLine();
                             Console.WriteLine("");
+                            //Opretter en et nyt Object, som åbner klassen Password_Logic.cs
+                            LogicClass.Password_Logic tjekefterkrav_Object = new LogicClass.Password_Logic();
+                            //Opretter en ny int, som er går ind i min public string "HasSpecialChar" med værdien fra stringen newpass
+                            int hasSpecialChar = tjekefterkrav_Object.HasSpecialChar(adgangskode);
+                            //Opretter en ny int, som er går ind i min public string "HasNumber" med værdien fra stringen newpass
+                            int hasNumber = tjekefterkrav_Object.HasNumber(adgangskode);
+                            //Opretter en ny int, som er går ind i min public string "HasCapital" med værdien fra stringen newpass
+                            int hasCapital = tjekefterkrav_Object.HasCapital(adgangskode);
 
-                            if (brugernavn != null && adgangskode.Length >= 12)
+
+                            //Læs alle linjer fra stringen filePathbrugernavn, hvis den indebærer input fra brugernavn og alle linjer fra stringen FilePathAdgangskode indebærer-
+                            //input fra adgangskode, så kør denne if sætning
+                            if (brugernavn != null && adgangskode.Length >= 12 && hasSpecialChar > 1 && hasNumber > 1 && hasCapital > 1)
                             {
+                                //Laver adgangskodekrav til true, så man bryder ud af loopen
                                 adgangskodekrav = true;
                                 string adgangskode2 = "";
+                                //Laver en while loop som kører hvis adgangskode ikke er den samme som adgangskode2 
                                 while (adgangskode != adgangskode2)
                                 {
                                     Console.Write("Gentast adgangskode: ");
                                     adgangskode2 = Console.ReadLine();
-
+                                    
+                                    //Kør denne her, hvis adgangskode er det samme som adgangskode2
                                     if (adgangskode == adgangskode2)
                                     {
+                                        //Opretter en string, som indebærer tekstfilen "brugernavn.txt" og opretter en string, som indebærer tekstfilen "adgangskode.txt"
                                         filePathBrugernavn = @"C:\Users\luczie\Desktop\The Three Cases - CSharp\Tre_Cases\Data\brugernavn.txt"; filePathAdgangskode = @"C:\Users\luczie\Desktop\The Three Cases - CSharp\Tre_Cases\Data\adgangskode.txt";
+                                        //Skriver alt tekst fra adgangskode ind i og erstatter teskten i stringen filePathAdgangskode 
                                         File.WriteAllText(filePathAdgangskode, adgangskode);
+                                        //Skriver alt tekst fra adgangskode ind i og erstatter teskten i stringen filePathAdgangskode 
                                         File.WriteAllText(filePathBrugernavn, brugernavn);
-
-
-
 
                                         Console.Clear();
                                         Console.WriteLine("Du har nu oprettet en ny bruger!");
                                         Console.WriteLine("Tryk Enter for, at navigere tilbage til login menuen");
                                         Console.ReadKey();
                                     }
+                                    //Hvis adgangskode ikke er det samme som adgangskode2, så kør denne her.
                                     else
                                     {
                                         Console.WriteLine("");
@@ -156,6 +228,7 @@ namespace Tre_Cases
                                 }
 
                             }
+                            //Kør denne hvis kravene fra hasSpecialChar, hasNumber og hasCapital ikke opfyldes
                             else
                             {
                                 Console.WriteLine("");
@@ -163,13 +236,14 @@ namespace Tre_Cases
                                 Console.WriteLine("");
                                 Console.WriteLine("Dit brugernavn må ikke være tomt");
                                 Console.WriteLine("Din adgangskode skal minimun være 12 karakterer lang");
-                                Console.WriteLine("Din adgangskode skal minimun indebære 2 speciale karakterer");
+                                Console.WriteLine("Din adgangskode skal minimun indebære 2 forskellige speciale karakterer");
+                                Console.WriteLine("Din adgangskode skal minimun indebære 2 forskellige tal");
 
                                 Console.ReadKey();
                             }
                         }
                         break;
-
+                    //Kør denne, hvis ingen af de cases i switchen ikke udfæres
                     default:
                         Console.Clear();
                         Console.WriteLine("Der er opstået en fejl");
@@ -180,6 +254,7 @@ namespace Tre_Cases
 
             }
 
+            //Åbner public classen program
             Console.Clear();
             var program = new Program();
             program.Run();
